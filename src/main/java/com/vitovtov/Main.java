62bf280789df35cb4bdc83cfe8822014255867d1
Yury <81083827+VitovtovY@users.noreply.github.com>
2023-09-12 00:59:03 +0700

    Update Main.java

--- src/main/java/com/vitovtov/Main.java
+++ src/main/java/com/vitovtov/Main.java
@@ -2,6 +2,6 @@ package com.vitovtov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Yuraaaa!");
+        System.out.println("Hello Yuraaaa!!!!!!!!!!");
     }
 }
